1ae94122f60661f8eae788d3f858fb20c16adb5f
Arafa-microsys <33137170+Arafa-microsys@users.noreply.github.com>
2019-06-19 00:21:26 +0200

    Add files via upload

---
+++ HelloKeypad.ino
@@ -0,0 +1,27 @@
+//https://www.youtube.com/arafamicrosystems
+#include <Keypad.h>
+
+const byte ROWS = 4; //four rows
+const byte COLS = 4; //three columns
+char keys[ROWS][COLS] = {
+  {'1','2','3','A'},
+  {'4','5','6','B'},
+  {'7','8','9','C'},
+  {'*','0','#','D'}
+};
+byte rowPins[ROWS] = {9, 8, 7, 6}; //connect to the row pinouts of the keypad
+byte colPins[COLS] = {5, 4, 2, 3}; //connect to the column pinouts of the keypad
+
+Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
+
+void setup(){
+  Serial.begin(9600);
+}
+  
+void loop(){
+  char key = keypad.getKey();
+  
+  if (key){
+    Serial.println(key);
+  }
+}
